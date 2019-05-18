dbafca9b090ef7630beb4e02582038ac414e7ab7
tru17189 <32879914+tru17189@users.noreply.github.com>
2019-05-18 13:39:37 -0600

    Update simulador.py

--- Fase 2 y 3/simulador.py
+++ Fase 2 y 3/simulador.py
@@ -21,22 +21,41 @@ miFrame.pack()
 leFrame.pack()
 
 #Propiedades del Frame
-miFrame.config(width="1250", height="500")
+miFrame.config(width="700", height="300")
 miFrame.config(bg="white")
 miFrame.config(relief="sunken")
-leFrame.config(width="1250", height="500")
+leFrame.config(width="700", height="300")
 leFrame.config(bg="red3")
 leFrame.config(relief="sunken")
 
+def ingresar_fecha():
+    #fecha para el query
+    FechaActual = str(year.get())+"-"+str(month.get())+"-"+str(day.get())
+    print(FechaActual)
+    
+
 #Comienzo de la estructura
 #titulo
-
 labelT = Label(miFrame, text="Simular Dia", fg= "red", font="Times")
-labelT.place(x=555, y=10)
+labelT.place(x=255, y=10)
+#Botones de fecha
+labelYear= Label(leFrame, text="Año", fg="white", bg="red3")
+labelYear.place(x=20, y=20)
+year = Spinbox(leFrame, from_=0, to=1000000, width=10)
+year.place(x=20, y=50)
 
+labelMonth= Label(leFrame, text="Mes", fg="white", bg="red3")
+labelMonth.place(x=20, y=80)
+month = Spinbox(leFrame, from_=0, to=12, width=10)
+month.place(x=20, y=110)
 
+labelDay= Label(leFrame, text="Dia", fg="white", bg="red3")
+labelDay.place(x=20, y=140)
+day = Spinbox(leFrame, from_=0, to=31, width=10)
+day.place(x=20, y=170)
 
- 
+fechaBoton = Button(leFrame, text="FECHA A SIMULAR", command=ingresar_fecha)
+fechaBoton.place(x=20, y=200)
 
 root.mainloop()
 cur.close()
